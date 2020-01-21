d96bbb2218c17ea8894faab80326a18bf60bd026
Bottegamarcos <strieter.caroll@gmail.com>
2020-01-21 12:00:00 +0000

    Add wallet logic part 225

--- wallet.py
+++ wallet.py
@@ -1118,3 +1118,8 @@ class Blockchain:
                     )
                     block.nonce = block_data['nonce']
                     block.hash = block_data['hash']
+                    self.chain.append(block)
+
+    def get_latest_block(self) -> Block:
+        return self.chain[-1]
+
