59ab0bca9d7a39cd7da110faf5dfe076d5a386bf
Bottegamarcos <strieter.caroll@gmail.com>
2020-01-22 12:00:00 +0000

    Add wallet logic part 50

--- wallet.py
+++ wallet.py
@@ -243,3 +243,8 @@ class Blockchain:
         block.mine_block(self.difficulty)
         self.chain.append(block)
         self.pending_transactions = [Transaction("network", miner_address, 10.0)]
+        return block
+
+    def get_balance(self, address: str) -> float:
+        balance = 0.0
+        for block in self.chain:
