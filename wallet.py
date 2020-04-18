28537f336a67ef5b3fba98cdd39d60381b9f7511
Bottegamarcos <strieter.caroll@gmail.com>
2020-04-18 12:00:00 +0000

    Add wallet logic part 113

--- wallet.py
+++ wallet.py
@@ -334,3 +334,6 @@ def main():
 
         elif choice == '4':
             is_valid = wallet.blockchain.is_chain_valid()
+            print(f"Blockchain valid: {is_valid}")
+
+        elif choice == '5':
