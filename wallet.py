6eb65d0e67c4217852a4c1d8cf877e86543f5cde
Bottegamarcos <strieter.caroll@gmail.com>
2020-06-29 12:00:00 +0000

    Add wallet logic part 471

--- wallet.py
+++ wallet.py
@@ -2348,3 +2348,8 @@ class Blockchain:
                         'amount': tx.amount,
                         'timestamp': datetime.fromtimestamp(tx.timestamp).strftime('%Y-%m-%d %H:%M:%S'),
                         'block_index': block.index
+                    })
+        return history
+
+class Network:
+    def __init__(self):
