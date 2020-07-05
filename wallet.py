814c71e54fc16167cb0399722038e821650fb6f9
Bottegamarcos <strieter.caroll@gmail.com>
2020-07-05 12:00:00 +0000

    Add wallet logic part 104

--- wallet.py
+++ wallet.py
@@ -204,3 +204,5 @@ class Block:
     def compute_hash(self) -> str:
         block_data = json.dumps({
             'index': self.index,
+            'transactions': [tx.to_dict() for tx in self.transactions],
+            'previous_hash': self.previous_hash,
