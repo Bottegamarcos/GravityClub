205d050eb20c2fb55384269b123abfdd1abedfd9
Bottegamarcos <strieter.caroll@gmail.com>
2020-07-07 12:00:00 +0000

    Add wallet logic part 282

--- wallet.py
+++ wallet.py
@@ -1403,3 +1403,8 @@ class Blockchain:
         self.difficulty = difficulty
         self.chain_file = chain_file
         self.load_chain()
+        if not self.chain:
+            self.create_genesis_block()
+
+    def create_genesis_block(self):
+        genesis_block = Block(0, [], "0")
