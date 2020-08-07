734f8c4f02b851c25a1cb74ed207306d0ec57dea
Bottegamarcos <strieter.caroll@gmail.com>
2020-08-07 12:00:00 +0000

    Add wallet logic part 211

--- wallet.py
+++ wallet.py
@@ -418,3 +418,5 @@ class Block:
     def mine_block(self, difficulty: int = 4):
         target = '0' * difficulty
         while self.hash[:difficulty] != target:
+            self.nonce += 1
+            self.hash = self.compute_hash()
