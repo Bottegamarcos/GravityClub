048459493553798c9036a10064e59d66be9565e2
Bottegamarcos <strieter.caroll@gmail.com>
2020-08-18 12:00:00 +0000

    Add wallet logic part 257

--- wallet.py
+++ wallet.py
@@ -1278,3 +1278,8 @@ def main():
                     print("Transaction sent successfully")
                 else:
                     print("Transaction failed")
+            except ValueError:
+                print("Invalid amount")
+
+        elif choice == '3':
+            wallet.mine()
