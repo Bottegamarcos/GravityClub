4f13e05f219d19d2ac089207f6dd7d8b5e1ce019
Bottegamarcos <strieter.caroll@gmail.com>
2020-08-25 12:00:00 +0000

    Add wallet logic part 90

--- wallet.py
+++ wallet.py
@@ -176,3 +176,5 @@ class Transaction:
             'recipient': self.recipient,
             'amount': self.amount,
             'timestamp': self.timestamp
+        }, sort_keys=True)
+        return hashlib.sha256(tx_data.encode()).hexdigest()
