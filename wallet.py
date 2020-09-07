37033e8866b37d639f956993e49a839bb152d5f6
Bottegamarcos <strieter.caroll@gmail.com>
2020-09-07 12:00:00 +0000

    Add wallet logic part 423

--- wallet.py
+++ wallet.py
@@ -1264,3 +1264,6 @@ def main():
         print("5. View transaction history")
         print("6. Recover wallet")
         print("7. Exit")
+        choice = input("Enter choice (1-7): ")
+
+        if choice == '1':
