91c20c3ad0c22af030654475a27aae2c50a1ce8c
Bottegamarcos <strieter.caroll@gmail.com>
2021-01-13 12:00:00 +0000

    Add wallet logic part 329

--- wallet.py
+++ wallet.py
@@ -654,3 +654,5 @@ def main():
                 print("No transactions found")
 
         elif choice == '6':
+            wallet_file = input("Enter wallet file path (default: wallet.pkl): ") or "wallet.pkl"
+            if os.path.exists(wallet_file):
