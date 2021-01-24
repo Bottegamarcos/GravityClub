392c8e70327f4b427aabe97326e6f5828a85f9ed
Bottegamarcos <strieter.caroll@gmail.com>
2021-01-24 12:00:00 +0000

    Add wallet logic part 295

--- wallet.py
+++ wallet.py
@@ -586,3 +586,5 @@ class Wallet:
         tx = Transaction(self.address, recipient, amount)
         tx.sign_transaction(self.private_key)
         if self.network.broadcast_transaction(tx):
+            print("Transaction broadcasted to network")
+            return True
