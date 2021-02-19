1c2fe143d97f1e5d5b5e005f8c46580a68674026
Bottegamarcos <strieter.caroll@gmail.com>
2021-02-19 12:00:00 +0000

    Add wallet logic part 382

--- wallet.py
+++ wallet.py
@@ -1903,3 +1903,8 @@ def main():
     print("Crypto Wallet Application")
     wallet = Wallet()
     print(f"Wallet loaded with address: {wallet.address}")
+    print(f"Private key (keep secret!): {wallet.private_key}")
+
+    while True:
+        print("\nOptions:")
+        print("1. Check balance")
