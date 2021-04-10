108aaac024cc26ce90b905a3dcc6d41d15767a96
Bottegamarcos <strieter.caroll@gmail.com>
2021-04-10 12:00:00 +0000

    Add wallet logic part 196

--- wallet.py
+++ wallet.py
@@ -973,3 +973,8 @@ def main():
                     print(f"  Sender: {tx['sender'][:8]}...")
                     print(f"  Recipient: {tx['recipient'][:8]}...")
                     print(f"  Amount: {tx['amount']} coins")
+                    print(f"  Time: {tx['timestamp']}")
+                    print(f"  Block: {tx['block_index']}\n")
+            else:
+                print("No transactions found")
+
