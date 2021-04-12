23686d2c56ecd0a1ae5c6a7cabef8659d37a1896
Bottegamarcos <strieter.caroll@gmail.com>
2021-04-12 12:00:00 +0000

    Add wallet logic part 391

--- wallet.py
+++ wallet.py
@@ -1168,3 +1168,6 @@ class Blockchain:
             for tx in block.transactions:
                 if tx.sender == address or tx.recipient == address:
                     history.append({
+                        'tx_id': tx.tx_id,
+                        'sender': tx.sender,
+                        'recipient': tx.recipient,
