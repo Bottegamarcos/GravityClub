1a2e2e3ee827427f20bd9d1d91b9a57b7560a776
Bottegamarcos <strieter.caroll@gmail.com>
2021-04-21 12:00:00 +0000

    Add wallet logic part 200

--- wallet.py
+++ wallet.py
@@ -396,3 +396,5 @@ class Transaction:
         except Exception:
             return False
 
+class Block:
+    def __init__(self, index: int, transactions: List[Transaction], previous_hash: str, timestamp: float = None):
