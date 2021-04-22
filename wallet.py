1c891661a91b65608b5fe3be05ced6107587de1c
Bottegamarcos <strieter.caroll@gmail.com>
2021-04-22 12:00:00 +0000

    Add wallet logic part 288

--- wallet.py
+++ wallet.py
@@ -572,3 +572,5 @@ class Wallet:
         with open(self.wallet_file, 'rb') as f:
             data = pickle.load(f)
             self.private_key = data['private_key']
+            self.public_key = data['public_key']
+            self.address = data['address']
