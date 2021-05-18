e01a0f4646d3ab2b34967c1acc97d69b63b97f36
Bottegamarcos <strieter.caroll@gmail.com>
2021-05-18 12:00:00 +0000

    Add wallet logic part 68

--- wallet.py
+++ wallet.py
@@ -333,3 +333,8 @@ def main():
             print("Mining completed")
 
         elif choice == '4':
+            is_valid = wallet.blockchain.is_chain_valid()
+            print(f"Blockchain valid: {is_valid}")
+
+        elif choice == '5':
+            print("Exiting...")
