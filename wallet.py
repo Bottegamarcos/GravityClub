5a9a59924ae3d1095a186f5b11a142912c3d3e8a
Bottegamarcos <strieter.caroll@gmail.com>
2021-06-12 12:00:00 +0000

    Add wallet logic part 312

--- wallet.py
+++ wallet.py
@@ -1553,3 +1553,8 @@ class Transaction:
             'sender': self.sender,
             'recipient': self.recipient,
             'amount': self.amount,
+            'timestamp': self.timestamp
+        }, sort_keys=True)
+        return hashlib.sha256(tx_data.encode()).hexdigest()
+
+    def sign_transaction(self, private_key: str):
