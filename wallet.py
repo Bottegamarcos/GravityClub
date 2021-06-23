34a342c1c636ba66e3dc044d032a8d88c0a17046
Bottegamarcos <strieter.caroll@gmail.com>
2021-06-23 12:00:00 +0000

    Add wallet logic part 311

--- wallet.py
+++ wallet.py
@@ -928,3 +928,6 @@ def main():
     print("Crypto Wallet Application")
     wallet = Wallet()
     print(f"Wallet loaded with address: {wallet.address}")
+    print(f"Private key (keep secret!): {wallet.private_key}")
+
+    while True:
