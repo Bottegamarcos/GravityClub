3aa39279f83c4360718db771d83b83853d3136a6
Bottegamarcos <strieter.caroll@gmail.com>
2021-06-26 12:00:00 +0000

    Add wallet logic part 217

--- wallet.py
+++ wallet.py
@@ -646,3 +646,6 @@ def main():
                 for tx in history:
                     print(f"ID: {tx['tx_id'][:8]}...")
                     print(f"  Sender: {tx['sender'][:8]}...")
+                    print(f"  Recipient: {tx['recipient'][:8]}...")
+                    print(f"  Amount: {tx['amount']} coins")
+                    print(f"  Time: {tx['timestamp']}")
