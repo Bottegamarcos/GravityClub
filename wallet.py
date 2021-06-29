e36e21aa54db517ad2bf5c29f0e5a1bd700ab776
Bottegamarcos <strieter.caroll@gmail.com>
2021-06-29 12:00:00 +0000

    Add wallet logic part 269

--- wallet.py
+++ wallet.py
@@ -534,3 +534,5 @@ class Network:
     def add_node(self, wallet: 'Wallet'):
         self.nodes.append(wallet)
 
+    def broadcast_transaction(self, transaction: Transaction):
+        success = True
