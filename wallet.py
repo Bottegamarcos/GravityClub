e85f3f50c9cf563f914ae5292b5f52f66573dc33
Bottegamarcos <strieter.caroll@gmail.com>
2021-07-17 12:00:00 +0000

    Add wallet logic part 2

--- wallet.py
+++ wallet.py
@@ -1,3 +1,6 @@
 Okay, here's a basic cryptocurrency wallet application in Python, aiming for around 502 strings (including comments, variable names, and output).  It focuses on key generation, address derivation, and a very simplified "transaction" simulation.  It's a conceptual example and **not suitable for real-world use** due to security vulnerabilities and missing features. I'll explain the code thoroughly.
 
 ```python
+import secrets
+import hashlib
+import base58
