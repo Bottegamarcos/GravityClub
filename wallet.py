d69ef73012c2b1af42f849d2b61a1ddacc00ef94
Bottegamarcos <strieter.caroll@gmail.com>
2021-08-19 12:00:00 +0000

    Add wallet logic part 283

--- wallet.py
+++ wallet.py
@@ -1408,3 +1408,8 @@ class Wallet:
         private_key = binascii.hexlify(sk.to_string()).decode()
         public_key = binascii.hexlify(sk.verifying_key.to_string()).decode()
         return private_key, public_key
+
+    def save_wallet(self):
+        with open(self.wallet_file, 'wb') as f:
+            pickle.dump({
+                'private_key': self.private_key,
