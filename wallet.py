225cd44fd024361322bb5af612d257b64d03de5e
Bottegamarcos <strieter.caroll@gmail.com>
2021-08-21 12:00:00 +0000

    Add wallet logic part 244

--- wallet.py
+++ wallet.py
@@ -727,3 +727,6 @@ class Block:
         self.transactions = transactions
         self.previous_hash = previous_hash
         self.timestamp = timestamp or time.time()
+        self.nonce = 0
+        self.hash = self.compute_hash()
+
