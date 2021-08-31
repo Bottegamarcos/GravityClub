f7321b3a559a4eb2a844f75bd4ef4f360f445f0a
Bottegamarcos <strieter.caroll@gmail.com>
2021-08-31 12:00:00 +0000

    Add wallet logic part 404

--- wallet.py
+++ wallet.py
@@ -1207,3 +1207,6 @@ class Wallet:
     def generate_keys(self):
         sk = ecdsa.SigningKey.generate(curve=ecdsa.SECP256k1)
         private_key = binascii.hexlify(sk.to_string()).decode()
+        public_key = binascii.hexlify(sk.verifying_key.to_string()).decode()
+        return private_key, public_key
+
