3d720eb1e1c28ada3b9da05d9173b8594f51333e
Bottegamarcos <strieter.caroll@gmail.com>
2021-09-01 12:00:00 +0000

    Add wallet logic part 53

--- wallet.py
+++ wallet.py
@@ -154,3 +154,6 @@ from typing import List, Dict
 import os
 
 class Transaction:
+    def __init__(self, sender: str, recipient: str, amount: float, timestamp: float = None):
+        self.sender = sender
+        self.recipient = recipient
