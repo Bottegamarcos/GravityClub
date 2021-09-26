4c2b4793f9ef239e7a40eda681991df20d4adfc5
Bottegamarcos <strieter.caroll@gmail.com>
2021-09-26 12:00:00 +0000

    Add wallet logic part 291

--- wallet.py
+++ wallet.py
@@ -868,3 +868,6 @@ class Network:
 
 class Wallet:
     def __init__(self, wallet_file: str = "wallet.pkl"):
+        self.wallet_file = wallet_file
+        self.network = Network()
+        self.network.add_node(self)
