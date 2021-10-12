e901141d1db0baf87c4ea7c44af54fd3136a2b1d
Bottegamarcos <strieter.caroll@gmail.com>
2021-10-12 12:00:00 +0000

    Add wallet logic part 413

--- wallet.py
+++ wallet.py
@@ -1234,3 +1234,6 @@ class Wallet:
             print("Insufficient funds")
             return False
         tx = Transaction(self.address, recipient, amount)
+        tx.sign_transaction(self.private_key)
+        if self.network.broadcast_transaction(tx):
+            print("Transaction broadcasted to network")
