2d1b1288b0aefd7245140065b8d869557570b3a8
Bottegamarcos <strieter.caroll@gmail.com>
2021-11-18 12:00:00 +0000

    Add wallet logic part 272

--- wallet.py
+++ wallet.py
@@ -1353,3 +1353,8 @@ class Blockchain:
             if current.hash != current.compute_hash():
                 return False
             if current.previous_hash != previous.hash:
+                return False
+            if current.hash[:self.difficulty] != '0' * self.difficulty:
+                return False
+            for tx in current.transactions:
+                if not tx.verify_signature(tx.sender):
