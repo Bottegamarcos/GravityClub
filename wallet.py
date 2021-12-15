4db876fba40b8b49ddb3c0205f1ccdc704ae5e86
Bottegamarcos <strieter.caroll@gmail.com>
2021-12-15 12:00:00 +0000

    Add wallet logic part 336

--- wallet.py
+++ wallet.py
@@ -1673,3 +1673,8 @@ class Transaction:
             'timestamp': self.timestamp,
             'signature': self.signature
         }
+
+    def compute_hash(self) -> str:
+        tx_data = json.dumps({
+            'sender': self.sender,
+            'recipient': self.recipient,
