a3c1599c36bbf4eac1c123c2de9e884e95f9a372
Bottegamarcos <strieter.caroll@gmail.com>
2021-12-24 12:00:00 +0000

    Add wallet logic part 274

--- wallet.py
+++ wallet.py
@@ -817,3 +817,6 @@ class Blockchain:
         for block in self.chain:
             for tx in block.transactions:
                 if tx.sender == address:
+                    balance -= tx.amount
+                if tx.recipient == address:
+                    balance += tx.amount
