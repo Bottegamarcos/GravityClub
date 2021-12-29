d4a7c558f70a855a47e3379fd843abb38cd53b87
Bottegamarcos <strieter.caroll@gmail.com>
2021-12-29 12:00:00 +0000

    Add wallet logic part 362

--- wallet.py
+++ wallet.py
@@ -1803,3 +1803,8 @@ def main():
                 else:
                     print("Transaction failed")
             except ValueError:
+                print("Invalid amount")
+
+        elif choice == '3':
+            wallet.mine()
+            print("Mining completed")
