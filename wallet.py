b494545a81cfe762a3adef78ceef21e4b1dcb9d9
Bottegamarcos <strieter.caroll@gmail.com>
2022-01-18 12:00:00 +0000

    Add wallet logic part 22

--- wallet.py
+++ wallet.py
@@ -40,3 +40,5 @@ def hash160(data):
     """Calculates the RIPEMD160 hash of the SHA256 hash of the data."""
     sha256_hash = hashlib.sha256(data).digest()
     ripemd160_hash = hashlib.new('ripemd160', sha256_hash).digest()
+    return ripemd160_hash
+
