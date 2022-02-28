29d8ecc24825a56cc1925d384f0c0cda22318ff7
Bottegamarcos <strieter.caroll@gmail.com>
2022-02-28 12:00:00 +0000

    Add wallet logic part 199

--- wallet.py
+++ wallet.py
@@ -592,3 +592,6 @@ class Wallet:
 
     def mine(self):
         block = self.blockchain.mine_pending_transactions(self.address)
+        print(f"Block #{block.index} mined with hash: {block.hash}")
+        return block
+
