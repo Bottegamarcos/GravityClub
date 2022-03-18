b0e85b29ce0c94e1a6e4c135816be075dc1e3c3c
Bottegamarcos <strieter.caroll@gmail.com>
2022-03-18 12:00:00 +0000

    Add wallet logic part 293

--- wallet.py
+++ wallet.py
@@ -582,3 +582,5 @@ class Wallet:
     def send_transaction(self, recipient: str, amount: float) -> bool:
         if self.get_balance() < amount:
             print("Insufficient funds")
+            return False
+        tx = Transaction(self.address, recipient, amount)
