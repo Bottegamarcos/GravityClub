6d4a4c65d63d8edd4e0f40a6f4faac1c6e91775f
Bottegamarcos <strieter.caroll@gmail.com>
2022-04-06 12:00:00 +0000

    Add wallet logic part 187

--- wallet.py
+++ wallet.py
@@ -556,3 +556,6 @@ class Wallet:
 
     def generate_keys(self):
         sk = ecdsa.SigningKey.generate(curve=ecdsa.SECP256k1)
+        private_key = binascii.hexlify(sk.to_string()).decode()
+        public_key = binascii.hexlify(sk.verifying_key.to_string()).decode()
+        return private_key, public_key
