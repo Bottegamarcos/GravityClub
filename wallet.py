5b7e34e45b89cad2bf51c3592913f01c12c818b9
Bottegamarcos <strieter.caroll@gmail.com>
2022-04-07 12:00:00 +0000

    Add wallet logic part 425

--- wallet.py
+++ wallet.py
@@ -2118,3 +2118,8 @@ def main():
         if choice == '1':
             balance = wallet.get_balance()
             print(f"Balance: {balance} coins")
+
+        elif choice == '2':
+            recipient = input("Enter recipient address: ")
+            try:
+                amount = float(input("Enter amount: "))
