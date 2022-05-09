a8795e9b2a17a37a7bbe1558134d6a0fc041310f
Bottegamarcos <strieter.caroll@gmail.com>
2022-05-09 12:00:00 +0000

    Add wallet logic part 176

--- wallet.py
+++ wallet.py
@@ -873,3 +873,8 @@ class Wallet:
         self.network.add_node(self)
         if os.path.exists(wallet_file):
             self.load_wallet()
+        else:
+            self.private_key, self.public_key = self.generate_keys()
+            self.address = self.public_key
+            self.blockchain = Blockchain()
+            self.save_wallet()
