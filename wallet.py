652b8d23ee0a04ad0757a5dd542049f0ed398a83
Bottegamarcos <strieter.caroll@gmail.com>
2022-06-18 12:00:00 +0000

    Add wallet logic part 231

--- wallet.py
+++ wallet.py
@@ -1148,3 +1148,8 @@ class Blockchain:
         return balance
 
     def is_chain_valid(self) -> bool:
+        for i in range(1, len(self.chain)):
+            current = self.chain[i]
+            previous = self.chain[i-1]
+            if current.hash != current.compute_hash():
+                return False
