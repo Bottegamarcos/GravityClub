4a31fa5ed1eb24d42c44dce328a6d798ff900d03
Bottegamarcos <strieter.caroll@gmail.com>
2022-07-02 12:00:00 +0000

    Add wallet logic part 49

--- wallet.py
+++ wallet.py
@@ -238,3 +238,8 @@ class Blockchain:
             return True
         return False
 
+    def mine_pending_transactions(self, miner_address: str):
+        block = Block(len(self.chain), self.pending_transactions, self.get_latest_block().hash)
+        block.mine_block(self.difficulty)
+        self.chain.append(block)
+        self.pending_transactions = [Transaction("network", miner_address, 10.0)]
