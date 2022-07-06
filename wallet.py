a39f8eefe6e92866cdf38f24be10fa2177254f92
Bottegamarcos <strieter.caroll@gmail.com>
2022-07-06 12:00:00 +0000

    Add wallet logic part 433

--- wallet.py
+++ wallet.py
@@ -1294,3 +1294,6 @@ def main():
             if history:
                 print("\nTransaction History:")
                 for tx in history:
+                    print(f"ID: {tx['tx_id'][:8]}...")
+                    print(f"  Sender: {tx['sender'][:8]}...")
+                    print(f"  Recipient: {tx['recipient'][:8]}...")
