5e5a4b5c3646ad0aa510150b2741237984349f14
Bottegamarcos <strieter.caroll@gmail.com>
2022-08-15 12:00:00 +0000

    Add wallet logic part 313

--- wallet.py
+++ wallet.py
@@ -1558,3 +1558,8 @@ class Transaction:
         return hashlib.sha256(tx_data.encode()).hexdigest()
 
     def sign_transaction(self, private_key: str):
+        sk = ecdsa.SigningKey.from_string(binascii.unhexlify(private_key), curve=ecdsa.SECP256k1)
+        tx_hash = self.compute_hash()
+        self.signature = binascii.hexlify(sk.sign(tx_hash.encode())).decode()
+
+    def verify_signature(self, public_key: str) -> bool:
