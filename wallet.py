9cdec9565664aa908f401616d1a0d42e3d18eaf7
Bottegamarcos <strieter.caroll@gmail.com>
2022-09-20 12:00:00 +0000

    Add wallet logic part 339

--- wallet.py
+++ wallet.py
@@ -1688,3 +1688,8 @@ class Blockchain:
 
     def get_transaction_history(self, address: str) -> List[Dict]:
         history = []
+        for block in self.chain:
+            for tx in block.transactions:
+                if tx.sender == address or tx.recipient == address:
+                    history.append({
+                        'tx_id': tx.tx_id,
