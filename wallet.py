3e73339fe2ed0d7ca1749a8d962ba75709f42dbc
Bottegamarcos <strieter.caroll@gmail.com>
2022-09-27 12:00:00 +0000

    Add wallet logic part 10

--- wallet.py
+++ wallet.py
@@ -25,3 +25,6 @@ def private_key_to_wif(private_key):
 def wif_to_private_key(wif):
     """Converts a WIF string back to a private key."""
     decoded_wif = base58.b58decode(wif)
+    if len(decoded_wif) != 36:
+        raise ValueError("Invalid WIF format")
+    if decoded_wif[0] != 0x80:
