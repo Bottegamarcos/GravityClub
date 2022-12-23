ca860caac2a8f38885d082706acf3a1e37fa9a73
Bottegamarcos <strieter.caroll@gmail.com>
2022-12-23 12:00:00 +0000

    Add wallet logic part 36

--- wallet.py
+++ wallet.py
@@ -103,3 +103,6 @@ def main():
     try:
         amount = float(input("Enter amount to send: "))
         wallet.send_transaction(recipient_address, amount)
+    except ValueError:
+        print("Invalid amount. Please enter a number.")
+
