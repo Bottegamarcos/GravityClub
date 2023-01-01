63091b4aa969dd574dc895b5c63709ddc0e085dd
Bottegamarcos <strieter.caroll@gmail.com>
2023-01-01 12:00:00 +0000

    Add wallet logic part 80

--- wallet.py
+++ wallet.py
@@ -156,3 +156,5 @@ import os
 class Transaction:
     def __init__(self, sender: str, recipient: str, amount: float, timestamp: float = None):
         self.sender = sender
+        self.recipient = recipient
+        self.amount = amount
