43b8bf01d555df9a8802ef34abe25c60d07b41cc
Bottegamarcos <strieter.caroll@gmail.com>
2023-01-11 12:00:00 +0000

    Add wallet logic part 498

--- wallet.py
+++ wallet.py
@@ -2483,3 +2483,8 @@ def main():
                 wallet = Wallet(wallet_file)
                 print(f"Wallet recovered with address: {wallet.address}")
             else:
+                print("Wallet file not found")
+
+        elif choice == '7':
+            print("Exiting...")
+            break
