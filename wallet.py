ad3b663784ecf7e51c56a3067ec3edadfff2124d
Bottegamarcos <strieter.caroll@gmail.com>
2023-01-18 12:00:00 +0000

    Add wallet logic part 491

--- wallet.py
+++ wallet.py
@@ -2448,3 +2448,8 @@ def main():
             recipient = input("Enter recipient address: ")
             try:
                 amount = float(input("Enter amount: "))
+                if wallet.send_transaction(recipient, amount):
+                    print("Transaction sent successfully")
+                else:
+                    print("Transaction failed")
+            except ValueError:
