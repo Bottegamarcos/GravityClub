bdb0012fca3e751bc23ccc4ffe450ebb73a84590
Bottegamarcos <strieter.caroll@gmail.com>
2023-03-16 12:00:00 +0000

    Add wallet logic part 92

--- wallet.py
+++ wallet.py
@@ -271,3 +271,6 @@ class Blockchain:
         return True
 
 class Wallet:
+    def __init__(self):
+        self.private_key, self.public_key = self.generate_keys()
+        self.address = self.public_key  # Simplified: using public key as address
