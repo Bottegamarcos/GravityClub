7e3109baf2155ca9f5998df7a72a052db51df875
Bottegamarcos <strieter.caroll@gmail.com>
2023-04-05 12:00:00 +0000

    Add wallet logic part 209

--- wallet.py
+++ wallet.py
@@ -622,3 +622,6 @@ def main():
 
         elif choice == '2':
             recipient = input("Enter recipient address: ")
+            try:
+                amount = float(input("Enter amount: "))
+                if wallet.send_transaction(recipient, amount):
