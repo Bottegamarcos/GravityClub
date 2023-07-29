1f290a58fcc6917a2ca04610ccd2d940f5ff1e3c
Bottegamarcos <strieter.caroll@gmail.com>
2023-07-29 12:00:00 +0000

    Add wallet logic part 389

--- wallet.py
+++ wallet.py
@@ -1938,3 +1938,8 @@ class Blockchain:
 
     def save_chain(self):
         with open(self.chain_file, 'w') as f:
+            json.dump([{
+                'index': block.index,
+                'transactions': [tx.to_dict() for tx in block.transactions],
+                'previous_hash': block.previous_hash,
+                'timestamp': block.timestamp,
