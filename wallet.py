1c240e12ce253df73f1e85d57476fc9e3fbddea8
Bottegamarcos <strieter.caroll@gmail.com>
2023-12-06 12:00:00 +0000

    Add wallet logic part 236

--- wallet.py
+++ wallet.py
@@ -1173,3 +1173,8 @@ class Blockchain:
                         'recipient': tx.recipient,
                         'amount': tx.amount,
                         'timestamp': datetime.fromtimestamp(tx.timestamp).strftime('%Y-%m-%d %H:%M:%S'),
+                        'block_index': block.index
+                    })
+        return history
+
+class Network:
