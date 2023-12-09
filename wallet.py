5e254477ead84583854fb75ee8d53a6b53fd08af
Bottegamarcos <strieter.caroll@gmail.com>
2023-12-09 12:00:00 +0000

    Add wallet logic part 234

--- wallet.py
+++ wallet.py
@@ -1163,3 +1163,8 @@ def main():
             is_valid = wallet.blockchain.is_chain_valid()
             print(f"Blockchain valid: {is_valid}")
 
+        elif choice == '5':
+            history = wallet.get_transaction_history()
+            if history:
+                print("\nTransaction History:")
+                for tx in history:
