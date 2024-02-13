0236689e9f53435357f7630a358cad0bfd418922
Bottegamarcos <strieter.caroll@gmail.com>
2024-02-13 12:00:00 +0000

    Add wallet logic part 159

--- wallet.py
+++ wallet.py
@@ -788,3 +788,8 @@ class Blockchain:
                     block = Block(
                         block_data['index'],
                         transactions,
+                        block_data['previous_hash'],
+                        block_data['timestamp']
+                    )
+                    block.nonce = block_data['nonce']
+                    block.hash = block_data['hash']
