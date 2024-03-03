8dd7d220682651a7e196108044cbfb8ef6062565
Bottegamarcos <strieter.caroll@gmail.com>
2024-03-03 12:00:00 +0000

    Add wallet logic part 88

--- wallet.py
+++ wallet.py
@@ -259,3 +259,6 @@ class Blockchain:
         for i in range(1, len(self.chain)):
             current = self.chain[i]
             previous = self.chain[i-1]
+            if current.hash != current.compute_hash():
+                return False
+            if current.previous_hash != previous.hash:
