20278b8894ee574d2cdbbbe48ca8e46b2119f0c3
Bottegamarcos <strieter.caroll@gmail.com>
2024-05-01 12:00:00 +0000

    Add wallet logic part 251

--- wallet.py
+++ wallet.py
@@ -748,3 +748,6 @@ class Block:
 
 class Blockchain:
     def __init__(self, difficulty: int = 4, chain_file: str = "blockchain.json"):
+        self.chain: List[Block] = []
+        self.pending_transactions: List[Transaction] = []
+        self.difficulty = difficulty
