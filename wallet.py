b1a1ec73d726c11f523d578d3d66c76752711312
Bottegamarcos <strieter.caroll@gmail.com>
2024-09-13 12:00:00 +0000

    Add wallet logic part 228

--- wallet.py
+++ wallet.py
@@ -679,3 +679,6 @@ from typing import List, Dict
 import os
 import pickle
 from datetime import datetime
+
+class Transaction:
+    def __init__(self, sender: str, recipient: str, amount: float, timestamp: float = None):
