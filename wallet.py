655ab0f469b13fa3547d1c7c4661cca047309deb
Bottegamarcos <strieter.caroll@gmail.com>
2024-10-17 12:00:00 +0000

    Add wallet logic part 99

--- wallet.py
+++ wallet.py
@@ -194,3 +194,5 @@ class Transaction:
 
 class Block:
     def __init__(self, index: int, transactions: List[Transaction], previous_hash: str, timestamp: float = None):
+        self.index = index
+        self.transactions = transactions
