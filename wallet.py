0239fa41c84ae266c01c7672bed058cc4db8dd36
Bottegamarcos <strieter.caroll@gmail.com>
2024-10-17 12:00:00 +0000

    Add wallet logic part 115

--- wallet.py
+++ wallet.py
@@ -226,3 +226,5 @@ class Blockchain:
 
     def create_genesis_block(self):
         genesis_block = Block(0, [], "0")
+        genesis_block.mine_block(self.difficulty)
+        self.chain.append(genesis_block)
