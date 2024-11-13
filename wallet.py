aa35dc3840666efcd518d664dccac903fdbe974b
Bottegamarcos <strieter.caroll@gmail.com>
2024-11-13 12:00:00 +0000

    Add wallet logic part 245

--- wallet.py
+++ wallet.py
@@ -1218,3 +1218,8 @@ class Transaction:
             'tx_id': self.tx_id,
             'sender': self.sender,
             'recipient': self.recipient,
+            'amount': self.amount,
+            'timestamp': self.timestamp,
+            'signature': self.signature
+        }
+
